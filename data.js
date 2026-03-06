24bf197c1cf6c16796a3a3e4f68b4c38786a218c
agarwal409-oss <230233638+agarwal409-oss@users.noreply.github.com>
2026-03-06 07:27:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ data.js
@@ -0,0 +1,16 @@
+const blogPosts = [
+    {
+        id: "nimbus-2026",
+        title: "Building the Team Metamorph Website",
+        date: "Feb 15, 2026",
+        image: "nimbus.jpg",    
+        content: "Detailed text about the NIMBUS 2026 tech fest goes here..."
+    },
+    {
+        id: "campus-life",
+        title: "A Day in the Life at NIT Hamirpur",
+        date: "Feb 10, 2026",
+        image: "campus.jpg",
+        content: "Vlog summary and insights into my daily routine..."
+    }
+];
